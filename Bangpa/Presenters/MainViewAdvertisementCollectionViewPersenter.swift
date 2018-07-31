55f8a07b613970236d6cc9b1f3925e5d9b389995
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-01 00:31:44 +0900

    MainView's Presenter

---
+++ Bangpa/Presenters/MainViewAdvertisementCollectionViewPersenter.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewAdvertisementCollectionViewPersenter.swift
+//  Bangpa
+//
+//  Created by 이동건 on 01/08/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import Foundation
+
+protocol AdvertisementCollectionViewProtocol: class {
+    
+}
+
+class MainViewAdvertisementCollectionViewPersenter {
+    private weak var collectionView: AdvertisementCollectionViewProtocol?
+}
