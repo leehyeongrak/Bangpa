dfa0879bf5e275f7891b622d4ddfe175898bcda0
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-22 00:37:15 +0900

    MapVCPresenter for MapViewController

--- Bangpa/ViewControllers/MapViewController.swift
+++ Bangpa/ViewControllers/MapViewController.swift
@@ -11,14 +11,8 @@ import UIKit
 class MapViewController: UIViewController {
     
     private var mapView: NMapView!
+    private var mapVCPresenter = MapVCPresenter(with: NMapLocationManager.getSharedInstance())
     @IBOutlet weak var changeStateButton: UIButton!
-    
-    enum state {
-        case disabled
-        case tracking
-    }
-    
-    var currentState: state = .disabled
     
     static func initFromStoryboard() -> UINavigationController {
         let storyboard = UIStoryboard(name: MapViewController.reusableIdentifier, bundle: nil)
@@ -28,6 +22,7 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapVCPresenter.attachView(vc: self)
         setupMapView()
     }
     
@@ -39,11 +34,15 @@ class MapViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        disableLocationUpdate()
+        mapVCPresenter.disableLocationUpdate()
     }
     
     @IBAction func searchCompleteButtonDidTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        mapVCPresenter.setupState()
     }
     
     fileprivate func setupMapView() {
@@ -93,37 +92,28 @@ extension MapViewController: NMapViewDelegate, NMapPOIdataOverlayDelegate {
     }
 }
 
-extension MapViewController: NMapLocationManagerDelegate {
-    open func locationManager(_ locationManager: NMapLocationManager!, didUpdateTo location: CLLocation!) {
-        
-        let coordinate = location.coordinate
-        
-        let myLocation = NGeoPoint(longitude: coordinate.longitude, latitude: coordinate.latitude)
-        let locationAccuracy = Float(location.horizontalAccuracy)
-        
-        mapView?.mapOverlayManager.setMyLocation(myLocation, locationAccuracy: locationAccuracy)
-        mapView?.setMapCenter(myLocation)
+extension MapViewController: MapViewProtocol {
+    func updateState(_ newState: TrackingState) {
+        switch newState {
+        case .disabled:
+            changeStateButton?.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_normal"), for: .normal)
+        case .tracking:
+            changeStateButton?.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_selected"), for: .normal)
+        }
     }
     
-    open func locationManager(_ locationManager: NMapLocationManager!, didFailWithError errorType: NMapLocationManagerErrorType) {
-        
-        var message: String = ""
-        
-        switch errorType {
-        case .unknown: fallthrough
-        case .canceled: fallthrough
-        case .timeout:
-            message = "일시적으로 내위치를 확인 할 수 없습니다."
-        case .denied:
-            message = "위치 정보를 확인 할 수 없습니다.\n사용자의 위치 정보를 확인하도록 허용하시려면 위치서비스를 켜십시오."
-        case .unavailableArea:
-            message = "현재 위치는 지도내에 표시할 수 없습니다."
-        case .heading:
-            message = "나침반 정보를 확인 할 수 없습니다."
-        }
-        
+    func clearMyLocation() {
+        mapView?.mapOverlayManager.clearMyLocationOverlay()
+    }
+    
+    func setupMyLocation(location: NGeoPoint, locationAccuracy: Float) {
+        mapView?.mapOverlayManager.setMyLocation(location, locationAccuracy: locationAccuracy)
+        mapView?.setMapCenter(location)
+    }
+    
+    func showLocaitonFailError(title: String, message: String) {
         if (!message.isEmpty) {
-            let alert = UIAlertController(title:"NMapViewer", message: message, preferredStyle: .alert)
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title:"OK", style:.default, handler: nil))
             present(alert, animated: true, completion: nil)
         }
@@ -132,85 +122,4 @@ extension MapViewController: NMapLocationManagerDelegate {
             mapView.setAutoRotateEnabled(false, animate: true)
         }
     }
-    
-    func locationManager(_ locationManager: NMapLocationManager!, didUpdate heading: CLHeading!) {
-        let headingValue = heading.trueHeading < 0.0 ? heading.magneticHeading : heading.trueHeading
-        setCompassHeadingValue(headingValue)
-    }
-    
-    func onMapViewIsGPSTracking(_ mapView: NMapView!) -> Bool {
-        return NMapLocationManager.getSharedInstance().isTrackingEnabled()
-    }
-    
-    func findCurrentLocation() {
-        enableLocationUpdate()
-    }
-    
-    func setCompassHeadingValue(_ headingValue: Double) {
-        
-        if let mapView = mapView, mapView.isAutoRotateEnabled {
-            mapView.setRotateAngle(Float(headingValue), animate: true)
-        }
-    }
-    
-    // MARK: - My Location
-    
-    func enableLocationUpdate() {
-        
-        if let lm = NMapLocationManager.getSharedInstance() {
-            
-            if lm.locationServiceEnabled() == false {
-                locationManager(lm, didFailWithError: .denied)
-                return
-            }
-            
-            if lm.isUpdateLocationStarted() == false {
-                // set delegate
-                lm.setDelegate(self)
-                // start updating location
-                lm.startContinuousLocationInfo()
-            }
-        }
-    }
-    
-    func disableLocationUpdate() {
-        
-        if let lm = NMapLocationManager.getSharedInstance() {
-            
-            if lm.isUpdateLocationStarted() {
-                // start updating location
-                lm.stopUpdateLocationInfo()
-                // set delegate
-                lm.setDelegate(nil)
-            }
-        }
-        
-        mapView?.mapOverlayManager.clearMyLocationOverlay()
-    }
-    
-    // MARK: - Button Control
-    
-    @IBAction func buttonClicked(_ sender: UIButton) {
-        switch currentState {
-        case .disabled:
-            enableLocationUpdate()
-            updateState(.tracking)
-        case .tracking:
-            disableLocationUpdate()
-            updateState(.disabled)
-        }
-    }
-    
-    func updateState(_ newState: state) {
-        
-        currentState = newState
-        
-        switch currentState {
-        case .disabled:
-            changeStateButton?.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_normal"), for: .normal)
-        case .tracking:
-            changeStateButton?.setImage(#imageLiteral(resourceName: "v4_btn_navi_location_selected"), for: .normal)
-        }
-    }
-    
 }
